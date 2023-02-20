f7ac7a83bfb8b5b062d6a07e3f01ac12e9694277
Andrey Antukh <niwi@niwi.nz>
2023-02-20 12:44:35 +0100

    :recycle: Refactor websockets impl to use virtual threads
    Removing the use of core.async code and implement code using
    plain old and familiar synchronous code

--- backend/src/app/main.clj
+++ backend/src/app/main.clj
@@ -198,9 +198,8 @@
     ::mtx/metrics (ig/ref ::mtx/metrics)}
 
    ::mbus/msgbus
-   {:backend   (cf/get :msgbus-backend :redis)
-    :executor  (ig/ref ::wrk/executor)
-    :redis     (ig/ref ::rds/redis)}
+   {::wrk/executor  (ig/ref ::wrk/executor)
+    ::rds/redis     (ig/ref ::rds/redis)}
 
    :app.storage.tmp/cleaner
    {::wrk/executor (ig/ref ::wrk/executor)
